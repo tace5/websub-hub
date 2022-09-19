e2a2ff7001352b67539c9c18d6b5af388b4a2b3c
Ruben Hume <ruben.hume@live.se>
2022-09-19 21:26:50 +0200

    Created hub class

--- hub/hub.go
+++ hub/hub.go
@@ -1,1 +1,38 @@
 package hub
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type Hub struct {
+	topics map[string]*Topic
+}
+
+func NewHub() *Hub {
+	h := Hub{topics: map[string]*Topic{}}
+
+	return &h
+}
+
+func (hub Hub) createTopic(topicName string) {
+	hub.topics[topicName] = NewTopic()
+}
+
+func (hub Hub) subscribe(topicName string, callbackUrl string) {
+	topic := hub.topics[topicName]
+	topic.subscribe(callbackUrl)
+}
+
+func (hub Hub) publish(topicName string, msg string) {
+	topic := hub.topics[topicName]
+
+	data := map[string]string{"topic": topicName, "msg": msg}
+	jsonData, err := json.Marshal(data)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic.publish(jsonData)
+}
