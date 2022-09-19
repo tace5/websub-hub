9ef5c5a616b194d6db84d13965596f2361fc6822
Ruben Hume <ruben.hume@live.se>
2022-09-19 21:26:29 +0200

    Created class for topic

--- hub/topic.go
+++ hub/topic.go
@@ -1,1 +1,43 @@
 package hub
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type Topic struct {
+	wg          *sync.WaitGroup
+	subscribers []string
+}
+
+func NewTopic() *Topic {
+	t := Topic{
+		wg:          new(sync.WaitGroup),
+		subscribers: []string{},
+	}
+
+	return &t
+}
+
+func (topic Topic) subscribe(callbackUrl string) {
+	topic.wg.Add(1)
+
+	go func() {
+		topic.subscribers = append(topic.subscribers, callbackUrl)
+	}()
+
+	topic.wg.Wait()
+}
+
+func (topic Topic) publish(data []byte) {
+	buffer := bytes.NewBuffer(data)
+
+	for _, url := range topic.subscribers {
+		_, err := http.Post(url, "application/json", buffer)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+}
